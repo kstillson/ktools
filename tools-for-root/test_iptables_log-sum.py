e012b20f226a4e8e9255966db3d6575daec19412
Ken Stillson <ken@point0.net>
2022-02-13 10:03:43 -0500

    rename .log file to avoid git ignore

--- tools-for-root/test_iptables_log-sum.py
+++ tools-for-root/test_iptables_log-sum.py
@@ -3,7 +3,7 @@
 import iptables_log_sum as base
 
 def test1():
-    counter = base.generate_counter(['testdata/iptables-1.log'], '++', 9999)
+    counter = base.generate_counter(['testdata/iptables-1.testlog'], '++', 9999)
     print(f'@@ c={counter.counter}')
     assert(len(counter.counter)) == 2
     for key, count in counter.counter.items():
