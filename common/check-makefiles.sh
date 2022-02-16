32844415e0e2c4c7c36b0688f049254c52fe150e
Ken Stillson <ken@point0.net>
2022-02-15 23:18:55 -0500

    add check-makefiles (search for missing targets) trivial makefile fixes

---
+++ common/check-makefiles.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check to see if any files that look like source code files are present
+# but missing from a Makefile target specification.
+
+top_level_dir='ktools'
+
+if [[ $(basename $(pwd)) != "$top_level_dir" ]]; then
+    echo "please run from the $top_level_dir dir."
+    exit -1
+fi
+
+temp_files_found="/tmp/files_found"
+temp_targets_found="/tmp/targets_found"
+temp_targets_missing="/tmp/targets_missing"
+
+dirs_with_makefiles=$(ls -1 */Makefile | cut -d/ -f1)
+for dir in $dirs_with_makefiles; do
+    find $dir -type f -print | cut -d/ -f2- | egrep -v 'Makefile|\.md$|test_|testdata' | cut -d. -f1 | sort > $temp_files_found
+    makefile="$dir/Makefile"
+    egrep '_TARGETS :?=' $makefile | sed -e 's/#.*$//' | cut -d= -f2- | tr ' ' '\n' | cut -d. -f1 | sed -e '/^$/d' | sort > $temp_targets_found
+    cat $temp_files_found | fgrep -v -f $temp_targets_found > $temp_targets_missing
+    echo ""
+    if [[ -s $temp_targets_missing ]]; then
+	echo "$dir - files missing from Makefile targets:"
+	cat $temp_targets_missing
+    else
+	echo "$dir - ok"
+    fi
+done
+
+echo ""
+# rm $temp_files_found $temp_targets_found $temp_targets_missing
+exit 0
