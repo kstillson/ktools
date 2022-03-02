1f3306b2a61aab73ca8a5c959f10d78af954eefe
Ken Stillson <ken@point0.net>
2022-03-02 09:48:07 -0500

    add new time queue and tests

---
+++ circuitpy_lib/k_time_queue.py
@@ -0,0 +1,64 @@
+
+import time
+
+'''Implement a queue of events to run at particular times daily.
+
+Designed to run on systems without threading or timer support.  Compatible
+with non-blocking call style.  Just call TimeQueue.check() occasionally, and
+any events with times now in the past will fire.
+
+use_daymins param is mostly for testing, but can be used for any cases
+where you want to override real time.
+'''
+
+def daymins(h, m): return h * 60 + m
+
+def daymins_ts(ts): return ts.tm_hour * 60 + ts.tm_min
+
+
+class TimedEvent:
+    def __init__(self, time_hour, time_min, func, args=[], kwargs={}):
+        self.daymins = daymins(int(time_hour), int(time_min))
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+
+    def fire(self): return self.func(*self.args, **self.kwargs)
+
+    def is_past(self, use_daymins=None):
+        now = use_daymins or daymins_ts(time.localtime())
+        return now > self.daymins
+
+
+class TimeQueue:
+    def __init__(self, list_of_timed_events, use_daymins=None):
+        self.queue = sorted(list_of_timed_events, key=lambda i: i.daymins)
+        now = use_daymins or buildtime_ts(time.localtime())
+        self.last_check = now
+        self.next_event_index = 0
+        for i, event in enumerate(self.queue):
+            if not self.queue[i].is_past(now):
+                self.next_event_index = i
+                break
+
+    def check(self, use_daymins=None):
+        fired = 0
+        now = use_daymins or buildtime_ts(time.localtime())
+        if now < self.last_check:
+            # We've wrapped to the next day. Empty any left-over queue.
+            while self.next_event_index < len(self.queue):
+                self.queue[self.next_event_index].fire()
+                fired += 1
+                self.next_event_index += 1            
+            self.next_event_index = 0
+        self.last_check = now
+        if self.next_event_index >= len(self.queue):
+            return fired                       # Nothing more until tomorrow.
+        while self.queue[self.next_event_index].is_past(now):
+            self.queue[self.next_event_index].fire()
+            fired += 1
+            self.next_event_index += 1
+            if self.next_event_index > len(self.queue):
+                self.next_event_index = None   # Queue exhausted for today.
+                break
+        return fired
---
+++ circuitpy_lib/tests/test_k_time_queue.py
@@ -0,0 +1,68 @@
+
+import context_circuitpy_lib   # fixup Python include path
+
+import k_time_queue as Q
+
+
+VALUE = -1
+
+def setter(new_val):
+    global VALUE
+    VALUE = new_val
+
+
+def test_basics():
+    tq = Q.TimeQueue([
+        Q.TimedEvent(3, 0,   setter, [300]),
+        Q.TimedEvent(4, 0,   setter, [400]),
+        Q.TimedEvent(2, 0,   setter, [200]),
+        Q.TimedEvent(1, 0,   setter, [100]),
+        Q.TimedEvent(23, 59, setter, [2359])],
+                     use_daymins = Q.daymins(1, 30))
+
+    # Check that things got sorted correctly.
+    assert tq.queue[0].daymins == Q.daymins(1, 0)
+
+    # Check the next queued item is the 02:00 event, as the time we
+    # set during construction was 01:30.
+    assert tq.next_event_index == 1
+    assert tq.queue[1].daymins == Q.daymins(2, 0)
+
+    # Let's call again before 02:00 and confirm nothing changes.
+    assert tq.check(Q.daymins(1, 31)) == 0
+    assert VALUE == -1
+    assert tq.check(Q.daymins(1, 59)) == 0
+    assert VALUE == -1
+
+    # Okay, now let's try at 2:01, and confirm a single event fires.
+    assert tq.check(Q.daymins(2, 1)) == 1
+    assert VALUE == 200
+
+    # Make sure nothing else fires until 0300
+    assert tq.check(Q.daymins(2, 1)) == 0
+    assert VALUE == 200
+    assert tq.check(Q.daymins(2, 31)) == 0
+    assert VALUE == 200
+
+    # Now let's skip all the way until past 0400 and confirm 2 events fire.
+    assert tq.check(Q.daymins(4, 1)) == 2
+    assert VALUE == 400
+    assert tq.next_event_index == 4
+    
+    # Wrap around to a call the next morning before 0100 and confirm the
+    # final event ran, and we're queued for 0100.
+    assert tq.check(Q.daymins(0, 1)) == 1
+    assert VALUE == 2359
+    assert tq.next_event_index == 0
+
+    # And finally check that the 0100 event fires as expected.
+    assert tq.check(Q.daymins(1, 2)) == 1
+    assert VALUE == 100
+
+
+def test_oddities():
+    tq = Q.TimeQueue([], use_daymins = Q.daymins(1, 30))
+    assert tq.check(Q.daymins(1, 31)) == 0
+    assert tq.check(Q.daymins(23, 59)) == 0
+    assert tq.check(Q.daymins(4, 0)) == 0
+
