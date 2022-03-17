3d0b414faa71ec729f2f51cbaa218a3cdfc4da5f
Ken Stillson <ken@point0.net>
2022-03-17 00:42:22 -0400

    kmc: add retry loop and lots of env vars to control details test_kmc: add a proper integration level test with tls verification enabled keymaster: add system for generating, using, and not committing a self-signed cert km test: improve mechanism for selecting test database (makes launching test_kmc.sh easier)

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 
 ## ++ktools
-   - tfu/kmc: add retry loop
+   - Tplink: generalize & publish tplink.py and related tools (change to separate docker)
    - backups: generalize & publish rclonedock and rsnapshot configs
    - dns-and-dhcp: generalize & publish dnsdock
    - general wisdom: lots of writing
@@ -9,24 +9,27 @@
    - syslog: generalize & publish syslogdock
    - tools-for-root: anything to prune from "q" ?
    - tools-for-users: doc
-   - Tplink: generalize & publish tplink.py and related tools
-   - add neotree or at least extracted elements...?
+   - k_auth: tidy, doc, test, and add cmnd to hash
 
 ## General
    - new name for ktools ?  (Mauveine?  #8D029B)
 
-## MILESTONE: ready for peer review ...?
+## ---------- MILESTONE: ready for peer review ...?
 
 ## Prep for homectrl
    - tidy up pylib/k_gpio
      - https://learn.adafruit.com/cooperative-multitasking-in-circuitpython-with-asyncio?view=all
      - https://circuitpython.readthedocs.io/en/latest/shared-bindings/keypad/index.html
-   - add RPi.GPIO to circuitpy_sim
+   - add RPi.GPIO (i.e. buttons) to circuitpy_sim and k_gpio
       - graphical interface for inputs and outputs
-   - k_auth: tidy, doc, test, and add cmnd to hash
 
 ## Homectrl -> ktools
    - move homectrl to ktools and rebind to pylib
       - add tests based on circuitpy_sim
    - change to Makefile based, add remote installation
 
+## Other
+   - search for things that need to be rebound / updated to new ktools interfaces (e.g. art-projects)
+
+## homesec
+   - rebind away from django  (flask?  all the way down to ktools?)
--- tools-for-users/kmc.sh
+++ tools-for-users/kmc.sh
@@ -1,26 +1,65 @@
 #!/bin/bash
 # Keymanager client
 
-# TODO: add internal retry logic
+# ---------- command line flags
 
+# keymaster usually uses the naming convension "$hostname-$keyname".
+# To follow this convention, provide keyname as $1 and do not provide a $2.
+# To use a prefix other than "$hostname-", provide it as $2.
+# Include any prefix/keyname separator (e.g. "-") at the end of $2.
+# To skip using a prefix, provide "" as $2.
+# (Note that the prefix comes AFTER the suffix in the command line...  Yeah, yeah, so sue me..  No wait - please don't sue me.)
+#
+# The retrieved secret is send to stdout.  Any errors go to stderr.
+# Exit status: 0 for success, or the curl error status for the most recent try.
 
-HOSTNAME=$(hostname)
-
-# $1 is the "scope", essentially the name of the key we're trying to retrieve.
-# By default, key's are named "$hostname-$scope".  Provide no $2 to follow this
-# convention.  If you use a different prefix, pass it as $2.  If you don't want
-# a prefix at all, pass "" as $2.
-SCOPE="$1"
-
-# Ken specific defaults, feel free to override from environment.
-if [[ "$HOSTNAME" == "jack" ]]; then KMHOST0="kmdock"; else KMHOST0="jack"; fi
-KMHOST="${KMHOST:-${KMHOST0}}"
-KMPORT="${KMPORT:-4443}"
+KEYNAME="$1"
 
 if [[ $# -gt 1 ]]; then
-  CONTEXT="$2"
+  PREFIX="$2"
 else
-  CONTEXT="${HOSTNAME}-"
+  PREFIX="$(/usr/bin/hostname)-"
 fi
 
-/usr/bin/wget -O - --no-check-certificate -q https://${KMHOST}:${KMPORT}/${CONTEXT}${SCOPE}
+# ---------- settings (can be overridden by the environment)
+
+CA_CERT_FILE="${CA_CERT_FILE:-km.crt}"   # set to "-" to skip TLS validation
+
+CURL_OPTS="${CURL_OPTS:--sS}"
+
+KMHOST="${KMHOST:-km}"
+KMPORT="${KMPORT:-4443}"
+
+RETRY_DELAY=${RETRY_DELAY:-5}     # seconds (delay between retries)
+RETRY_LIMIT=${RETRY_LIMIT:-9999}  # set to zero to output "" upon failure.
+
+TIMEOUT=${TIMEOUT:-5}            # seconds (how long to wait for each try)
+
+# ---------- computed settings
+
+URL="https://${KMHOST}:${KMPORT}/${PREFIX}${KEYNAME}"
+
+if [[ "$CA_CERT_FILE" == "-" ]]; then
+    TLS_OPT="--insecure"
+else
+    TLS_OPT="--cacert ${CA_CERT_FILE}"
+fi
+
+# ---------- main retry loop
+
+try=0
+secret=""
+while [[ $try -le $RETRY_LIMIT ]]; do 
+    ((try++))
+    secret=$(/usr/bin/curl $CURL_OPTS --connect-timeout $TIMEOUT $TLS_OPT $URL)
+    curl_status=$?
+    if [[ "$secret" != "" ]]; then
+	echo "$secret"
+	exit 0
+    fi
+    echo "$(/usr/bin/date): try #${try} failed (status $curl_status); will retry in $RETRY_DELAY" >&2
+    sleep $RETRY_DELAY
+done
+
+echo "retries exceeded" >&2
+exit $curl_status
