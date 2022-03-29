159d902bb9e7a20a3888fa521652c13ba17d5b9d
Ken Stillson <ken@point0.net>
2022-03-29 13:25:35 -0400

    fix relocated tests and move testdata under tests

--- tools-for-root/tests/test_iptables_log-sum.py
+++ tools-for-root/tests/test_iptables_log-sum.py
@@ -5,7 +5,7 @@ import context_tfr     # fix path to includes work as expected in tests
 import iptables_log_sum as base
 
 def test1():
-    counter = base.generate_counter(['testdata/iptables-1.testlog'], '++', 9999)
+    counter = base.generate_counter(['tests/testdata/iptables-1.testlog'], '++', 9999)
     assert(len(counter.counter)) == 2
     for key, count in counter.counter.items():
         if ' log-drop-fwd:' in key: expect = 1
