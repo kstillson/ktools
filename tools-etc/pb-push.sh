4331105ea54f57babe819b03489775389abfb2de
Ken Stillson <ken@point0.net>
2022-08-30 20:55:46 -0400

    add missing make dep needed to get pb-push into homesecdoc minor improvements to pb-push script (mostly doc)

--- tools-etc/pb-push.sh
+++ tools-etc/pb-push.sh
@@ -6,30 +6,26 @@
 
 # Reference doc:  https://docs.pushbullet.com/#create-push
 
-# TODO: rewrite in python3 and using kcore
-
 # Ken-specific note: "Tasker" app on Android phone uses contents to change
-# audible alert done.  Include "#a" to escalate to alert tone, or #i to
-# de-escalate to information-only notification tone.
+# audible alert done.  Prefix MSG with "#a," to escalate to alert tone, or
+# "#i," to de-escalate to information-only notification tone.
 
-MSG="$@"
+# Required params
+MSG="$@1"
+if [[ "$MSG" == "" ]]; then echo "Error- must provide message to send as params." >&2 ; exit 3; fi
 
+# Optional environment variable controls.
 DEFAULT_RATE_LIMIT=${DEFAULT_RATE_LIMIT:-2,240}  # Allow 2 pushes every 4 minutes.
-LOG=${PB_LOG:-/var/log/apache2/pb-push.log}
+LOG=${PB_LOG:-/var/log/homesec/pb-push.log}
 RL_FILE=${RL_FILE:-/tmp/pb.rl}
-
-# TODO: somewhat Ken specific.
-# Default LOG location is in the apache log dir.  But if that doesn't
-# exist, it indicates this script is being called from outside docker,
-# so instead use the full host path to the same file.
-if [[ ! -f "${LOG}" ]]; then
-  LOG="/rw/dv/webdock/var_log_apache2/pb-push.log"
-fi
+ACCESS_TOKEN=${PB_TOKEN}
 
 if [[ ! -f "${RL_FILE}" ]]; then ratelimiter -i $DEFAULT_RATE_LIMIT $RL_FILE; fi
 ratelimiter ${RL_FILE} || { { echo "$(date): RATELIMITED: $MSG" | tee -a $LOG; } >&2 ; exit 1; }
 
-ACCESS_TOKEN=$(/usr/local/bin/kmc pb-push)
+if [[ "$ACCESS_TOKEN" == "" ]]; then
+    ACCESS_TOKEN=$(/usr/local/bin/kmc pb-push)
+fi
 if [[ "$ACCESS_TOKEN" == "" || "$ACCESS_TOKEN" == *Error* ]]; then
     echo "unable to get access token" | tee -a $LOG >&2
     exit 2
@@ -41,3 +37,5 @@ curl -sS -m 5 --header "Access-Token: ${ACCESS_TOKEN}" \
      --data "{\"body\":\"${MSG}\",\"type\":\"note\"}" \
      --request POST -sS \
      https://api.pushbullet.com/v2/pushes  |& tee -a $LOG
+
+exit $?
