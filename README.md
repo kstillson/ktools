552faa7c0f8e370c15001605713b8d3f4006ad75
Ken Stillson <ken@point0.net>
2022-10-21 11:53:50 -0400

    varz: merge varz_prom into primary varz, controlled by a environment variable. readme: add list of environment variables that control various aspects of ktools

--- README.md
+++ README.md
@@ -176,6 +176,55 @@ Some highlights:
 
 - - -
 
+## Control variables
+
+A number of environment variables affect the way the system operates:
+
+- BUILD_SIMPLE: In the pylib/ directory, the default is to build and install
+  by building a Python "wheel" .whl file.  This is reasonably portable, but
+  requires pip3 and takes quite a bit longer.  Set BUILD_SIMPLE="1" to skip
+  the wheel and install the files directly by copying into place.
+
+- DBUILD_PARAMS: docker-infrastructure/d-build.sh will pass anything set here
+  through to the "docker build" command.  Not generally needed.
+
+- DBUILD_REPO: name of the docker repository that d-build will build into.
+  default is 'ktools'
+
+- DOCKER_BASE_DIR: If d-build is given a relative directory in-which to find a
+  container to build (via the --cd flag), this is the directory the reference
+  will be relative to.
+
+- HC_DATA_DIR: pylib/home-control/hc.py needs a bunch of data files to operate
+  (to provide device and scene data, and for plugins to load).  If set, this
+  variable provides a default location to search for those files.
+
+- KTOOLS_DRUN_LOG: default Docker log type to use for
+  docker-infrastructure/d-run.sh if not specified on the command-line or in
+  the container's settings file.
+
+- KTOOLS_DRUN_NETWORK: default Docker network to use when launching containers
+  from d-run, if not specified via flag or settings file.
+
+- KTOOLS_DRUN_REPO2: fallback Docker repository to use for d-run, if not
+  specified via flag or settings file.  Note that the primary repo to try
+  "ktools", but d-run will fall back to this if that doesn't work.  So if
+  you've got a personal repo, set it's value here.
+
+- KTOOLS_DRUN_TEST_NETWORK: docker Network to use when d-run is launching a
+  container in --dev or --dev_test mode, if not specified via flag.
+
+- KTOOLS_VARZ_PROM: set to "1" for kcore.varz to automatically export all
+  varz to Prometheus /metrics.  Requires the prometheus_client library
+  to be installed.
+
+- ROOT_RO: TODO(defer): {not fully implemented yet}
+  If set to "1", then ktools Makefile's assume that your root filesystem is
+  read-only, and that anything being installed there requires a temporary
+  remounting of the root-fs in "rw" mode.
+
+- - -
+
 ## My challenge to you
 
 If you ever find yourself with a system you're proud of, and you've got the IP
--- pylib/kcore/varz.py
+++ pylib/kcore/varz.py
@@ -1,22 +1,42 @@
 '''varz is a simple key/value singleton database used for tracking program state.
 
-A great Google engineering best-practice I try to maintain is all programs
-exporting a /varz web handler.  Use these functions to publish counters or
-other internal useful state.  For example, each time some important
-operation happens, call varz.inc('important-operation-#1').
+A great Google engineering best-practice is that O(all) programs export a
+/varz web handler.  Use these functions to publish counters or other internal
+useful state.  For example, each time some important operation happens, call
+varz.inc('important-operation-#1').
 
 This allows both humans and automated systems to easily check on how things
 are going with a service, and inspect program state without a debugger or
 digging into logs.
 
 Avoid putting sensitive data into varz, as it's not access controlled.
+
+If the environment variable $KTOOLS_VARZ_PROM == "1", then this module will
+also export varz (and optionally /healthz) as Prometheus /metrics.  This
+requires the prometheus_client library to be installed, and the init_prom()
+method to be called.
+
 '''
 
-import time
+import os, re, sys, time
+
+if os.environ.get('KTOOLS_VARZ_PROM') == '1':
+    USE_PROM = True
+    import prometheus_client as PC
+    import prometheus_client.exposition as PCE
+    import kcore.webserver_base as WB
+else:
+    USE_PROM = False
+
 
 # ---------- internal state
 
+PROGRAM_NAME = os.path.basename(sys.argv[0])
 VARZ = {}
+
+# Prometheus related
+HEALTHZ_HANDLER = None     # set by init_prom()
+PROM_INSTANCES = {}        # Maps from varz name to prometheus metric instances.
 
 
 # ---------- getters
@@ -29,18 +49,28 @@ def get(counter_name=None):
 
 def bump(counter_name): inc(counter_name, 1)
 
+
 def inc(counter_name, add=1):
     global VARZ
     if counter_name not in VARZ: VARZ[counter_name] = 0
     VARZ[counter_name] += add
+    if USE_PROM: _get_prom_instance(counter_name, PC.Counter).inc(add)
+
 
 def set(var_name, value):
     global VARZ
     VARZ[var_name] = value
+    if USE_PROM:
+        if isinstance(value, int) or isinstance(value, float):
+            _get_prom_instance(var_name, PC.Gauge).set(value)
+        else:
+            _get_prom_instance(var_name, PC.Info).info({'value': value})
+
 
 def stamp(stamp_name):  # Sets current epoch seconds.
     global VARZ
     VARZ[stamp_name] = int(time.time())
+    if USE_PROM: _get_prom_instance(stamp_name, PC.Counter).inc()
 
 
 # ---------- management
@@ -50,10 +80,66 @@ def reset(counter_name=None):
     if counter_name: VARZ[counter_name] = None
     else: VARZ = {}
 
-    
-def init(*unused_args, **unused_kwargs):
-    '''This is a no-op placeholder.  Initialization isn't needed for ths version
-       of this module, but it is in the varz_prom version, and I'm trying to keep
-       the API's interchangable.  No need to call this and no harm if you do.''' 
-    return True
+
+# ---------- Prometheus specific
+
+def init_prom(webserver, auto_healthz=True):
+    '''To register a /metrics handler, pass in an already constructed
+       kcore.webserver instance.  To also instrument /healthz, make sure any
+       custom /healthz handler is registered before calling.'''
+    if not USE_PROM: return
+    webserver.add_handler('/metrics', _metrics_handler)
+    if auto_healthz:
+        hd = webserver._find_handler('/healthz')
+        if hd:
+            global HEALTHZ_HANDLER
+            HEALTHZ_HANDLER = hd.func
+
+
+# ==========  INTERNALS
+
+def _get_prom_instance(varz_name, factory):
+    prefix = '' if varz_name.startswith('healthz') else 'varz_'
+    prom_name = prefix + re.sub(r'[^a-zA-Z0-9_:]', '_', varz_name)
+    global PROM_INSTANCES
+    if prom_name not in PROM_INSTANCES: PROM_INSTANCES[prom_name] = factory(prom_name, '', ['program'])
+    return PROM_INSTANCES[prom_name].labels(PROGRAM_NAME)
+
+
+def _metrics_handler(request):
+    # This is an adaptor that matches the kcore handler API, but makes use of
+    # prometheus_client.exposition.py:_bake_output() to perform the work.
+    # Essentially this is a translation of prometheus_client.exposition.do_GET().
+    #
+    # Yes, this is slimy and somewhat fragile, as you're not supposed to
+    # directly call PCE's leading underscore methods, as their signature might
+    # change.  But we can't call PC.exposition.do_GET because our instance of
+    # BaseHTTPRequestHandler has been lost by the time this handler method is
+    # called, and besides, the way kcore.webserver handlers output generation
+    # is rather incompatible.
+
+    # ----- auto_healthz
+
+    # If we know the webserver's /healthz handler, call it now, and translate
+    # it's output into metrics data.
+    if HEALTHZ_HANDLER:
+        out = HEALTHZ_HANDLER(request)
+        if isinstance(out, WB.Response): out = out.text
+        _get_prom_instance('healthz', PC.Info).info({'value': out})
+        status = 0 if (out.startswith('ok') or 'all ok' in out) else 1
+        _get_prom_instance('healthz_status', PC.Gauge).set(status)
+
+    # ----- Have prometheus_client.exposition generate the actual output.
+
+    registry = PC.REGISTRY
+    accept_header = request.headers.get('Accept')
+    params = request.get_params
+
+    status, header_tuple, output = PCE._bake_output(registry, accept_header, params)
+
+    # ----- Populate a Response object for our output.
+
+    code = int(status.split(' ', 1)[0])
+    headers = { header_tuple[0]: header_tuple[1] }
+    return WB.Response(output.decode('utf-8'), code, headers)
 
