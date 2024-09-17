5dbe80e94d77a69685fb9809a48bebb3064b0477
Ken Stillson <ken@point0.net>
2024-09-17 10:49:53 -0400

    update path

--- services/filewatch/filewatch_config.py
+++ services/filewatch/filewatch_config.py
@@ -45,7 +45,7 @@ CONFIG = {
     '/root/dv/rsnapdock/var_log/rsnapshot.log':    RSNAP_LOG_AGE,
     '/root/dv/webdock/var_log_apache2/access.log': SYS_LOG_AGE,
 
-    '/root/rsnap/daily.0/a4/rw/home/rubuntu/backup/var/log/syslog': RUBUNTU_LOG_MAX_AGE,
+    '/root/rsnap/daily.0/home/home/rubuntu/backup/var/log/syslog': RUBUNTU_LOG_MAX_AGE,
     
     # cron exceptions
     '/root/syslog/cron-black.log':                 3 * DAY,
